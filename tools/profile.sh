4a55c4b37f8ad5f9fbaa48d562ecbc31f7dae927
nicosobreira <nicolau.sobreira@gmail.com>
2026-01-10 22:14:05 -0300

    One run.sh script

--- tools/profile.sh
+++ tools/profile.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
 
 set -eou pipefail
-
-cmake --preset profile
-
-cmake --build build/profile
-
-perf record -g ./build/profile/app
-
-hotspot perf.data &>/dev/null &
--- tools/run.sh
+++ tools/run.sh
@@ -2,15 +2,59 @@
 
 set -eou pipefail
 
-BINARY="./build/app"
+function run_profile()
+{
+	local binary="./build/profile/App/app"
 
-cmake --preset debug
+	cmake --preset profile
 
-cmake --build build
+	cmake --build build/profile
 
-if [[ ${1:-} =~ "debug" ]]; then
-	./tools/debug.sh "$BINARY"
-	exit 0
-fi
+	perf record -g "$binary"
 
-exec "$BINARY"
+	hotspot perf.data &>/dev/null &
+}
+
+function run_debug()
+{
+	local binary="./build/App/app"
+	local local_host="1234"
+
+	cmake --preset debug
+	cmake --build build
+
+	if [[ ${TMUX+x} ]]; then
+		tmux split-window -h -p 50 "gdbserver :${local_host} ./${binary}"
+		tmux select-pane -t left
+		gdb -q --tui -ex "target remote :${local_host}"
+	else
+		gdb -q --tui "${binary}"
+	fi
+}
+
+function run()
+{
+	local binary="./build/App/app"
+
+	cmake --preset debug
+	cmake --build build
+
+	exec "$binary"
+}
+
+MODE="${1-}"
+
+case "$MODE" in
+	profile | p)
+		run_profile
+		;;
+	debug | d)
+		run_debug
+		;;
+	run | r)
+		run
+		;;
+	*)
+		run
+		;;
+esac
